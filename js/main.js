6a3141749b11720695dce23b32474fce8798e176
Hams Ouida <hamsouida202@gmail.com>
2018-02-21 05:22:24 +0200

    Popover and Popup Task
    when hover on an image, it is to be shown in a popover.
    and when click it should be opened in this modal view.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+// popover // 
+$(document).ready(function() {
+
+  $('[data-toggle="popover"]').popover({
+    html: true,
+    placement: 'auto',
+    trigger: 'hover',
+    content: function() {
+      var url = $(this).data('content');
+      return '<img src="' + url + '">'
+    }
+  });
+});
+
+
+/// popup ///
+var getImages = document.getElementsByTagName('img');
+for (var i = 0; i < getImages.length; i++) {
+  (function(x) {  
+    getImages[x].addEventListener('click', function() {
+
+      var modal = document.getElementById('popup');
+      var newImage = (this.getAttribute('src'));
+    
+      var modalImg = document.getElementById('newimg');
+      
+          modal.style.display = "block";
+          modalImg.src = newImage;
+
+     var span = document.getElementsByClassName("close")[0];
+     span.onclick = function() { 
+    modal.style.display = "none";
+}
+})
+  }
+  (i))
+}
+
+
